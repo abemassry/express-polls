91cb46942f0b1e50f7f7c0c7ecce9c076393c3eb
abemassry <abe@massindustries.org>
2014-07-01 12:44:42 -0400

    starting to fill out controller

--- routes/createnew.js
+++ routes/createnew.js
@@ -1,5 +1,12 @@
 var pact = require('../pact.js');
 
 exports.main = function(req, res){
+  var question = req.param('question');
+  var option0 = req.param('option0');
+  var option1 = req.param('option1');
+  var option2 = req.param('option2');
+  var option3 = req.param('option3');
+  var option4 = req.param('option4');
+  var option5 = req.param('option5');
   
 };
