dd25a038c54f2af6f9798af7339a24271067d9c0
abemassry <abe@massindustries.org>
2014-07-16 18:00:05 -0400

    adding poll view, working on styling

--- routes/poll.js
+++ routes/poll.js
@@ -5,19 +5,13 @@ exports.main = function(req, res){
   pact.db.get(req.params.id, function (err, value) {
     if (err) return console.log(req.params.id+' does not exist in poll.js');
     console.log(req.params.id+' found');
-    console.log(util.inspect(value, {showHidden: false, depth:null}));
-    console.dir(value);
-    console.log(value);
-
-    var voteData = [
-      { label: "Polls",  data: 20},
-      { label: "Learning",  data: 50},
-      { label: "Fun",  data: 30}
-    ];
-    res.render('index', { title: 'Express Polls - Poll', 
-                          voteData: voteData, 
-                          render: true
-                        }
+    //voteData: voteData,
+    var data = JSON.parse(value);
+    var question = data.question;
+    res.render('poll', { title: question, 
+                         data: data, 
+                         render: true
+                       }
     );
   });
 };
