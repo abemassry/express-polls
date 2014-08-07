adbccd7735194b2f4a9962ca0297304f0d2181f4
abemassry <abe@massindustries.org>
2014-08-07 18:09:43 -0400

    need to count up the votes for every question

--- routes/top.js
+++ routes/top.js
@@ -8,10 +8,19 @@ exports.main = function(req, res){
     .on('data', function (data) {
       var title = JSON.parse(data.value).question;
       var id = data.key.split('!');
+      pact.db.createReadStream({start: 'vote!'+pollId + '!',
+                                  end: 'vote!'+pollId + '!~'
+                              })
+        .on('data', function (data) {
+          var count;
+          for (var i = 0; i<data.value.
+
+
+
       polls.push({id: id[1], title: title});
     })
     .on('end', function() {
-      res.render('all', { title: 'All',
+      res.render('top', { title: 'Top',
                           polls: polls,
                           render: false
                          }
