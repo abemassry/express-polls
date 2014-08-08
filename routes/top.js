88f297221541b9a3056012f09d2b6bb661425d13
abemassry <abe@massindustries.org>
2014-08-08 18:11:22 -0400

    listing and sorting might have to be done using sync

--- routes/top.js
+++ routes/top.js
@@ -2,25 +2,33 @@ var pact = require('../pact.js')
 
 exports.main = function(req, res){
   var polls = new Array();
+  var count = 0;
   pact.db.createReadStream({start: 'poll!', 
                               end: 'poll!~'
                           })
     .on('data', function (data) {
       var title = JSON.parse(data.value).question;
-      var pollId = data.key.split('!');
+      var pollIdStr = data.key.split('!');
+      var pollId = pollIdStr[1];
       pact.db.createReadStream({start: 'vote!'+pollId + '!',
                                   end: 'vote!'+pollId + '!~'
                               })
         .on('data', function (data) {
-          var count;
-          for (var i = 0; i<data.value; i++) {
-            polls.push({id: id[1], title: title});
-          }
-
-
-      })
+          count++;
+        })
+        .on('end', function () {
+          polls.push({title: title, id: pollId, votes: count});
+          console.log(' ');
+          console.log('count: ');
+          console.log(count);
+          console.log(title);
+          console.log(pollId);
+          console.log(' ');
+          count = 0;
+        })
     })
     .on('end', function() {
+      console.log(polls);
       res.render('top', { title: 'Top',
                           polls: polls,
                           render: false
