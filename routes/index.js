99836edde9da11469e36c0b823376335c44a0040
abemassry <abe@massindustries.org>
2014-08-11 18:03:44 -0400

    array is sorting and displaying for top

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,36 @@
 var pact = require('../pact.js')
 
 exports.main = function(req, res){
-  var voteData = [
-    { label: "Polls",  data: 20},
-    { label: "Learning",  data: 50},
-    { label: "Fun",  data: 30}
-  ];
 
   res.render('index', { title: 'Express Polls - Home', 
-                        voteData: voteData, 
                         render: true
                       }
   );
 
 };
 
+
+//<h2>This site was made with:</h2>
+//<div id="minor" style="width: 100%; height: 363px;"></div>
+//<h3>100 Votes</h3>
+//  var minorData = [
+//    { label: "Luck",  data: 10},
+//    { label: "Skill",  data: 20},
+//    { label: "Concentrated Power of Will",  data: 15},
+//    { label: "Pleasure",  data: 5},
+//    { label: "Pain",  data: 50}
+//
+//  ];
+//  $.plot('#minor', minorData, {
+//    series: {
+//      pie: {
+//        show: true,
+//        radius: 11/16,
+//        innerRadius: 0.3
+//      }
+//    },
+//    legend: {
+//      show: false
+//    }
+//  });
+//
--- routes/top.js
+++ routes/top.js
@@ -12,24 +12,34 @@ exports.main = function(req, res){
       var id = keySplit[1];
       
       if (type === 'poll') {
-        polls[id] = { question: JSON.parse(data.value).question,
-                         count: 0
-                        };
+        polls[id] = { 
+          question: JSON.parse(data.value).question,
+          count: 0
+        };
       } else if (type === 'vote') {
         polls[id].count = polls[id].count + 1;
       }
+
     })
     .on('end', function() {
       var i = 0;
-      //for (var key in polls) {
-      //  list.push({
-        console.log(' ');
-        console.log('polls');
-        console.log(polls);
-      res.render('top', { title: 'Top',
-                          polls: polls,
-                          render: false
-                         }
-      );
+
+      for (var key in polls) {
+        list.push({ 
+          id: key,
+          title: polls[key].question,
+          votes: polls[key].count
+        });
+      }
+
+      list.sort(function(a, b) {
+        return parseInt(a.votes) - parseInt(b.votes)
+      });
+
+      res.render('top', { 
+        title: 'Top',
+        polls: list,
+        render: false
+      });
     })
 };
