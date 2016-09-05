07ff5900aea2dbb5a5fcbcc0334e6aad86d229d6
abemassry <abe@massindustries.org>
2016-09-05 16:14:24 -0400

    :tada: const and fat arrow works, yay!

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 //Module dependencies
 
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var expressLayouts = require('express-ejs-layouts');
-var flash = require('connect-flash');
+const express = require('express');
+const expressSession = require('express-session');
+const http = require('http');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const flash = require('connect-flash');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 
-var app = express();
-server = http.createServer(app);
-var io = require('socket.io').listen(server);
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
 exports.io = io;
 // 
 // From SO:
@@ -17,8 +21,8 @@ exports.io = io;
 // routes take the form of ex. routes.users.main
 // can have subroutes in each file
 //
-var routes = new Array();
-require("fs").readdirSync(path.join(__dirname, 'routes')).forEach(function(file) {
+const routes = [];
+require("fs").readdirSync(path.join(__dirname, 'routes')).forEach((file) => {
   if (file.match(/.+\.js$/g) !== null) {
     var name = file.replace('.js', '');
     routes[name] = require(path.join(__dirname, 'routes/') + file);
@@ -26,26 +30,33 @@ require("fs").readdirSync(path.join(__dirname, 'routes')).forEach(function(file)
 });
 
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.set('layout', 'layout');
-  app.use(expressLayouts);
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.cookieParser('keyboard cat'));
-  app.use(express.session({ cookie: { maxAge: 60000 }}));
-  app.use(flash());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.static(path.join(__dirname, 'public/javascripts')));
-  app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.send(500, 'Something broke!');
-  });
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('layout', 'layout');
+app.use(expressLayouts);
+app.use(expressSession({
+  secret: 'this_is_a_secret_here',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    path: '/',
+    httpOnly: true
+  }
+}));
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.cookieParser('keyboard cat'));
+app.use(express.session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/javascripts')));
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
 });
 
 app.configure('development', function(){
