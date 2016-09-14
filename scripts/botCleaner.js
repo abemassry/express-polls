bd60bab814db32ff4b89cd8438a909cd35a74ef1
Abe Massry <a@abemassry.com>
2016-09-13 23:05:16 -0400

    clean up after bots

---
+++ scripts/botCleaner.js
@@ -0,0 +1,21 @@
+const pact = require('../pact.js');
+
+pact.db.createReadStream({start: 'poll!',
+                            end: 'poll!~'
+                        })
+  .on('data', (data) => {
+    const title = JSON.parse(data.value).question;
+    if (title.match('href=')) {
+      pact.db.del(data.key, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    } else {
+      console.log(title);
+    }
+  })
+  .on('end', () => {
+    console.log('done');
+  });
+
